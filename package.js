85645b9468d32225255e72d29dbfd0e76715eda5
Aric Camarata <alisalaah@gmail.com>
2015-04-19 16:53:52 +0700

    Updated for Meteor 1+

--- package.js
+++ package.js
@@ -1,10 +1,16 @@
 Package.describe({
-    summary: "Session based geolocation api package that makes location information available via sessions"
+	name: 'alisalaah:session-location',
+	summary: 'Session based geolocation api package that makes location information available via sessions',
+	version: '2.0.2',
+	git: 'https://github.com/alisalaah/meteor-session-location.git'
 });
 
-Package.on_use(function (api) {
-	api.use('standard-app-packages', ['client', 'server']);
-	api.use('googlemaps', 'client');
+Package.onUse(function(api) {
+	api.versionsFrom('METEOR@0.9.2');
+	api.use(['ui'], 'client');
+	api.use(['blaze'], 'client');
+	api.use(['templating'], 'client');
+	api.use('mrt:googlemaps', 'client');
 	
 	api.add_files("location.js", "client");
     api.add_files("navform.css", "client");
